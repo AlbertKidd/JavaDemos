6b8910e088b4e4a739aaafd919d96c74752f25b6
Albert Kidd <475555354@qq.com>
2017-08-13 22:53:28 +0800

    add spring aop demos

---
+++ src/main/java/com/demos/aspectj/CustomAspect.java
@@ -0,0 +1,22 @@
+package com.demos.aspectj;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Log4j2
+@Aspect
+public class CustomAspect {
+
+    @Before("execution(public * *(..))")
+    public void before(JoinPoint joinPoint){
+        log.info(joinPoint + " is starting ------------------------");
+    }
+
+    @After("execution(public * *(..))")
+    public void after(JoinPoint joinPoint){
+        log.info(joinPoint + " is finished ------------------------");
+    }
+}
