fa3ff49ef02bd839ef2e0493d54aace715170501
Kidd <475555354@qq.com>
2017-08-14 18:26:06 +0800

    add msLink, log4j2 lookup demos

---
+++ src/main/java/com/demos/log4j/LogConnectionFactory.java
@@ -0,0 +1,57 @@
+package com.demos.log4j;
+
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDataSource;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author Kidd
+ *         CreateTime 2017/8/9.
+ */
+public class LogConnectionFactory {
+
+    private final DataSource dataSource;
+
+    private static interface Singleton{
+        final LogConnectionFactory INSTANCE = new LogConnectionFactory();
+    }
+
+    private LogConnectionFactory(){
+        this.dataSource = setupDatasource();
+    }
+
+    public static Connection getDatabaseConnection() throws SQLException {
+        return Singleton.INSTANCE.dataSource.getConnection();
+    }
+
+    private DataSource setupDatasource(){
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "admin");
+
+        try {
+            properties.load(new FileInputStream("src/main/resources/conf/jdbc.properties"));
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
+                "jdbc:mysql://127.0.0.1:3306/demos",
+                properties
+        );
+        PoolableConnectionFactory poolableConnectionFactory =
+                new PoolableConnectionFactory(connectionFactory, null);
+        GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>(poolableConnectionFactory);
+        poolableConnectionFactory.setPool(pool);
+        return new PoolingDataSource(pool);
+    }
+}
---
+++ src/test/java/com/demos/log4j/Log4j2Demo.java
@@ -0,0 +1,33 @@
+package com.demos.log4j;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.ThreadContext;
+import org.junit.Test;
+
+@Log4j2
+public class Log4j2Demo {
+
+
+    @Test
+    public void simpleLog(){
+
+        for (int i = 0; i < 100000; i++){
+            ThreadContext.put("context", "context is : " + i);
+            log.info("time" + i + "begin ____________________________");
+            log.trace("trace");
+            log.debug("debug");
+            log.info("info");
+            log.warn("warn");
+            log.error("error");
+//            log.fatal("fatal");
+            log.info("time" + i + "finish ____________________________");
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+                log.error(e.toString());
+            }
+            ThreadContext.clearMap();
+        }
+    }
+
+}
