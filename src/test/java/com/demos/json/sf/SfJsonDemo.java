2468ab9b50d858e57c3c87095a78f0a146ffe018
Kidd <475555354@qq.com>
2017-08-17 21:13:42 +0800

    1.add sf json demo 2.support gradle

--- src/test/java/com/demos/json/sf/SfJsonDemo.java
+++ src/test/java/com/demos/json/sf/SfJsonDemo.java
@@ -1,11 +1,9 @@
 package com.demos.json.sf;
 
-import com.demos.beans.PubUcustomer;
 import com.demos.beans.RegisterResult;
 import net.sf.json.JSONObject;
+import net.sf.json.util.JSONUtils;
 import org.junit.Test;
-
-import java.sql.Timestamp;
 
 /**
  * @author Kidd
@@ -15,12 +13,9 @@ public class SfJsonDemo {
 
     @Test
     public void test(){
-        RegisterResult registerResult = new RegisterResult();
-        registerResult.setSuccess(true);
-        registerResult.setErrorMsg("none");
-        PubUcustomer pubUcustomer = new PubUcustomer();
-        pubUcustomer.setEditdate(new Timestamp(System.currentTimeMillis()));
-        String s = "{\"success\":true,\"ucustomer\":{\"foreignKeys\":[],\"reg_machinename\":\"\",\"updateFlag\":\"Unchanged\",\"mobile\":\"\",\"ucustomerid\":\"00A\",\"memo\":\"\",\"credate\":{\"date\":14,\"hours\":20,\"seconds\":9,\"month\":7,\"nanos\":780000000,\"timezoneOffset\":-480,\"year\":117,\"minutes\":50,\"time\":1502715009780,\"day\":1},\"linkaddress\":\"\",\"linkman\":\"\",\"pidcode\":\"\",\"machinecode\":\"123\",\"qqno\":\"\",\"midcode\":\"\",\"options\":{},\"details\":[],\"allProperty\":{},\"customername\":\"\",\"editdate\":{\"date\":14,\"hours\":20,\"seconds\":9,\"month\":7,\"nanos\":780000000,\"timezoneOffset\":-480,\"year\":117,\"minutes\":50,\"time\":1502715009780,\"day\":1},\"email\":\"xxxxx\",\"properties\":{},\"wechatno\":\"\"},\"errorMsg\":\"\"}";
+        String s = "{\"success\":true,\"ucustomer\":{\"reg_machinename\":\"\",\"updateFlag\":\"Unchanged\",\"mobile\":\"\",\"ucustomerid\":\"00A\",\"memo\":\"\",\"credate\":\"1992-10-19 23:52:18\",\"linkaddress\":\"\",\"linkman\":\"\",\"pidcode\":\"\",\"machinecode\":\"123\",\"qqno\":\"\",\"midcode\":\"\",\"customername\":\"\",\"email\":\"xxxxx\",\"wechatno\":\"\"},\"errorMsg\":\"\"}";
         JSONObject jsonObject = JSONObject.fromObject(s);
+        JSONUtils.getMorpherRegistry().registerMorpher(new TimestampMorpher());
+        RegisterResult result = (RegisterResult)JSONObject.toBean(jsonObject, RegisterResult.class);
     }
 }
